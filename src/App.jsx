4f13441e3cd241209e3f357215b7492ecf24697a
yatharthyadu <yatharth.yadu66@gmail.com>
2025-03-29 00:36:41 +0530

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+function App() {
+  return (
+    <>
+      <h1 className="my-8 text-center text-5xl font-bold">Hello World</h1>
+      <h2>Checking</h2>
+ 
+    </>
+  );
+}
+
+export default App;
